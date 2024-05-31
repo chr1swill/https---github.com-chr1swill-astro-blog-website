f69261cc2ebfa26adcf0b0620a820ec6d382eaae
chr1swill <alexanderwilliams20003@gmail.com>
2024-05-31 12:20:01 -0400

    feat: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  routes: [
-    { path: '/', component: './src/pages/index.astro' },
-    { path: '/blog/*', component: './src/pages/blog/[post].md' }
-    // Add more routes as needed
-  ],
-  mount: {
-    public: '/',
-    src: '/_dist_',
-    // Add more mount points as needed
-  },
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: 'es2018',
-  },
-  // Add any additional configuration options here
+	routes: [
+		{ path: "/", component: "./src/pages/index.astro" },
+		{ path: "/blog/*", component: "./src/pages/blog/[post].md" },
+		// Add more routes as needed
+	],
+	mount: {
+		public: "/",
+		src: "/_dist_",
+		// Add more mount points as needed
+	},
+	optimize: {
+		bundle: true,
+		minify: true,
+		target: "es2018",
+	},
+	build: {
+		sitemap: true,
+	},
+	// Add any additional configuration options here
 });
