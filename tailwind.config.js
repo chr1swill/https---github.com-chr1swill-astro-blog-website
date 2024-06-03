66582fcaef1a9cbb0e681ad835426b9c194f3151
chr1swill <alexanderwilliams20003@gmail.com>
2024-06-03 18:24:14 -0400

    feat: update css realted stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js,tmpl}"],
+	content: ["./src/**/*.{html,js,templ}"],
 	theme: {
 		extend: {
 			colors: {
