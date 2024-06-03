81ddd4b411f2e4811054d055c1c5c244955b8e0c
chr1swill <alexanderwilliams20003@gmail.com>
2024-06-03 08:36:57 -0400

    feat: config css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js,tmpl}"],
+	theme: {
+		extend: {
+			colors: {
+				text: "var(--text)",
+				"text-op": "var(--text-op)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				surface: "var(--surface)",
+				accent: "var(--accent)",
+				background: "var(--background)",
+			},
+			fontFamily: {
+				base: "var(--font-family)",
+			},
+		},
+	},
+	plugins: [],
+};
