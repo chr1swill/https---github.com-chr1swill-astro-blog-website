68fd651abe04dc949c33111560397f4ac38396ef
chr1swill <alexanderwilliams20003@gmail.com>
2024-06-05 21:43:03 -0400

    fix: correct MIME error with serving css files

--- server/server.go
+++ server/server.go
@@ -6,25 +6,24 @@ import (
 )
 
 func main() {
+	cssFs := http.FileServer(http.Dir("./css"))
+	http.Handle("/css/", http.StripPrefix("/css/", cssFs))
 
-    cssFs := http.FileServer(http.Dir("./css"))
-    http.Handle("/css/", cssFs)
+	imagesFs := http.FileServer(http.Dir("./images"))
+	http.Handle("/images/", http.StripPrefix("/images/", imagesFs))
 
-    imagesFs := http.FileServer(http.Dir("./images"))
-    http.Handle("/images/", imagesFs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodGet)
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodGet {
-            w.Header().Set("Allow", http.MethodGet)
-            http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-            return
-        }
+		http.ServeFile(w, r, "dist/index.html")
+	})
 
-        http.ServeFile(w, r, "dist/index.html")
-    })
-
-    fmt.Println("Starting server on port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Server failed to start: ", err)
-    }
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server failed to start: ", err)
+	}
 }
